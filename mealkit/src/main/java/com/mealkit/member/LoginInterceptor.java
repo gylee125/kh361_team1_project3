db105adf0d088fdd069724de833aa0c60a6b3b05
김한얼 <ozkhan@naver.com>
2022-08-16 20:22:00 +0900

    인터셉터 경로 추가(상품 부분)
    관리자 아닌데, 상품 생성, 수정, 삭제 접근시 메인페이지로 보내버리기
    (조회는 가능하게)

--- mealkit/src/main/java/com/mealkit/member/LoginInterceptor.java
+++ mealkit/src/main/java/com/mealkit/member/LoginInterceptor.java
@@ -3,22 +3,21 @@ package com.mealkit.member;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class LoginInterceptor extends HandlerInterceptorAdapter{
-	
-	private static final Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
 	
 	 @Override
 	    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 	            throws Exception {
+		 	log.info("로그인 확인 인터셉트 작동!");
 	        HttpSession session = request.getSession();
 	        Object obj = session.getAttribute("member");
 	        if ( obj == null ){     
-	        	logger.info("인터셉트: 로그인 필요");
+	        	log.error("잘못된 접근 : 로그인 필요");
 	            response.sendRedirect(request.getContextPath() + "/");
 	            return false; 
 	        }       
