4958a846fdc9fb5ee192e5bcdad6b7c988da34cc
j5h006 <yj5h006@gmail.com>
2022-08-29 16:46:56 +0900

    adminPage.jsp 페이지 이동 수정

--- mealkit/src/main/java/com/mealkit/member/MemberListDTO.java
+++ mealkit/src/main/java/com/mealkit/member/MemberListDTO.java
@@ -1,15 +1,12 @@
 package com.mealkit.member;
 
 import java.util.Date;
-
 import lombok.Data;
-
-// 필요한지 검토하고 안 쓰면 지우기
 
 @Data
 public class MemberListDTO {
 	
-	private int mNo;
+	private int rn;
 	private String mId;
 	private String mName;
 	private String email;
